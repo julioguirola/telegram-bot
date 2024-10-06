c9fc409e277f91e8b9329be4d2f0a9189dd53948
julioguirola <julioguirola03@gmail.com>
2024-10-06 13:27:40 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+import os
+from dotenv import load_dotenv
+import requests
+import json
+from symbols import list_of_arb_sym
+
+def getPrice (symbol):
+    response = requests.get(f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}")
+    return json.loads(response.text)["price"]
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    for lista in list_of_arb_sym:
+        try:
+            a = getPrice(lista[0])
+            b = getPrice(lista[1])
+            c = getPrice(lista[2])
+
+            profit = 1 / float(a) * 1 / float(b) * float(c)
+
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=f"|{lista[0]} -> {lista[1]} -> {lista[2]}| = {profit}")
+        except:
+            continue
+
+
+
+if __name__ == '__main__':
+    load_dotenv()
+
+    token = os.getenv("TOKEN")
+
+    application = ApplicationBuilder().token(token).build()
+
+    start_handler = CommandHandler('start', start)
+
+    application.add_handler(start_handler)
+
+    application.run_polling()
