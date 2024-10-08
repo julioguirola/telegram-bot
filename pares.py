bba106acee7dc79cb7de28a02e19b8c2580fe49c
julioguirola <julioguirola03@gmail.com>
2024-10-08 15:32:07 -0400

    pares nuevos

--- pares.py
+++ pares.py
@@ -16,15 +16,6 @@ for coin in monedas:
         if coin == par.split()[0]:
             for par2 in symbolos:
                 if par.split()[0] == par2.split()[1] and par.split()[1] != par2.split()[0]:
-                    triangulos.append([par.replace(" ",""),f"{par.split()[1]} {par2.split()[0]}",par2.replace(" ","")])
+                    triangulos.append([par.replace(" ",""),f"{par.split()[1]} {par2.split()[0]}".replace(" ",""),par2.replace(" ","")])
 
-triangulos_ok = []
-
-
-for triangulo in triangulos:
-    for sym in symbolos:
-        if triangulo[1] == sym:
-            triangulos_ok.append([triangulo[0], sym.replace(" ", ""), triangulo[2]])
-
-
-print(triangulos_ok)
+print(triangulos)
